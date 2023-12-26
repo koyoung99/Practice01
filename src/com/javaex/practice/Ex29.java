6058fbfe1635eb0d83639ac65114085aecbc9b76
김아영 <72481734+koyoung99@users.noreply.github.com>
2023-12-26 15:32:34 +0900

    Update Ex29.java

--- src/com/javaex/practice/Ex29.java
+++ src/com/javaex/practice/Ex29.java
@@ -9,10 +9,11 @@ public class Ex29 {
 		Scanner sc = new Scanner(System.in);
 
 		System.out.print("가로를 입력하세요 : ");
-		System.out.print("세로를 입력하세요 : ");
-
 		double width = sc.nextDouble();
+		
+		System.out.print("세로를 입력하세요 : ");
 		double height = sc.nextDouble();
+		
 
 		double area = width * height;
 		double length = 2 * (width + height);
